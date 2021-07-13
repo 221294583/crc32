44b840e377b26e932cc0f49381e3aa88ced009bb
quxinna <quxinna@gmail.com>
2021-07-13 10:49:21 +0800

    Update vid.py

--- vid.py
+++ vid.py
@@ -1,7 +1,7 @@
-crc32_poly=0x104c11db7
-crc32_table=[]
+crc32_poly = 0x104c11db7
+crc32_table = []
 for operator in range(256):
-    operator<<=24
+    operator <<= 24
     for bit in range(8):
         if (operator & 0x80000000) != 0:
             operator <<= 1
@@ -9,15 +9,15 @@ for operator in range(256):
         else:
             operator <<= 1
     crc32_table.append(operator)
-to_print=list(map(hex,crc32_table))
+to_print = list(map(hex,crc32_table))
 print(to_print)
 def crc32(line):
-    var=0xffffffff
+    var = 0xffffffff
     for character in line:
-        operator=ord(character)
-        operator=int('{:08b}'.format(operator)[::-1],2)
-        operator=operator^(var>>24)
-        var=(crc32_table[operator])^(var<<8)&0xffffffff
-    var=int('{:032b}'.format(var)[::-1],2)
-    return var^0xffffffff
+        operator = ord(character)
+        operator = int('{:08b}'.format(operator)[::-1],2)
+        operator = operator ^ (var >> 24)
+        var = (crc32_table[operator]) ^ (var << 8) & 0xffffffff
+    var = int('{:032b}'.format(var)[::-1],2)
+    return var ^ 0xffffffff
 print(hex(crc32('123456789')))
