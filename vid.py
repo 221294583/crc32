5b33d69807f7f8aab3dd4fdb1b293564fcccf2cf
quxinna <quxinna@gmail.com>
2021-07-13 10:40:33 +0800

    Update vid.py

--- vid.py
+++ vid.py
@@ -1,23 +1,23 @@
-poly_crc32_normal=0x104c11db7
-crc32_table_normal=[]
+poly_crc32=0x104c11db7
+crc32_table=[]
 for operator in range(256):
     operator<<=24
     for bit in range(8):
         if (operator & 0x80000000) != 0:
             operator <<= 1
-            operator ^= poly_crc32_normal
+            operator ^= poly_crc32
         else:
             operator <<= 1
-    crc32_table_normal.append(operator)
-to_print_normal=list(map(hex,crc32_table_normal))
-print(to_print_normal)
-def crc32_normal(line):
+    crc32_table.append(operator)
+to_print=list(map(hex,crc32_table))
+print(to_print)
+def crc32(line):
     var=0xffffffff
     for character in line:
         operator=ord(character)
         operator=int('{:08b}'.format(operator)[::-1],2)
         operator=operator^(var>>24)
-        var=(crc32_table_normal[operator])^(var<<8)&0xffffffff
+        var=(crc32_table[operator])^(var<<8)&0xffffffff
     var=int('{:032b}'.format(var)[::-1],2)
     return var^0xffffffff
-print(hex(crc32_normal('123456789')))
+print(hex(crc32('123456789')))
